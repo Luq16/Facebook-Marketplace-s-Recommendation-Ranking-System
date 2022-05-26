fab95f9f187acc0281589e63506f88ad120c1109
Luq16 <luqawoniyi@gmail.com>
2022-05-26 17:56:46 +0100

    Import data

---
+++ clean_tabular.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("./data/Products.csv", lineterminator='\n')
+
+data
+
+
+/home/luqman/Desktop/AiCore/Facebook-Marketplace-s-Recommendation-Ranking-System/data/Products.csv
+
+
+
